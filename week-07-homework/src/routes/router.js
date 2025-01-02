895bffeb31f8c50e64c90092e1d447739d232ca7
Hyeonjae <hy0013@naver.com>
2025-01-02 16:31:26 +0900

    d

---
+++ week-07-homework/src/routes/router.js
@@ -0,0 +1,11 @@
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {});
+
+router.post("/sign");
+
+router.get("/users/:userId", async (req, res) => {});
+
+router.delete("/users/:userId", async (req, res) => {});
+
+router.patch("/users/:userId", async (req, res) => {});
